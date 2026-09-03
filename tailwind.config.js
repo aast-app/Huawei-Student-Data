7d62c89cfbca2e8a0267d2e339eb0e9c864de973
ToxicJoEz <latenightgamer20166@gmail.com>
2026-09-03 13:27:22 +0300

    fix: safelist tailwind colors for dynamic courses

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  safelist: [
+    {
+      pattern: /(bg|text)-(indigo|blue|sky|cyan|emerald|orange|red|fuchsia|pink|yellow|teal)-(50|600)/,
+    },
+  ],
   theme: {
     extend: {
       fontFamily: {
